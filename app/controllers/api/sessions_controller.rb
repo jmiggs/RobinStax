9e7ac790a37a710d23d96abee1a81bee45ce3bb9
miggs <jmigueldc95@gmail.com>
2020-04-27 15:28:28 -0700

    built backend routes and controllers for auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(
+          params[:user][:username],
+          params[:user][:password]
+        )
+    
+        if @user
+          login(@user)
+          render "api/users/show"
+        else
+          render json: ["You did not enter the correct information."], status: 401
+        end
+    end
+    
+    def destroy
+        @user = current_user
+        if @user
+          logout
+          render "api/users/show"
+        else
+          render json: ["Please Sign in"], status: 404
+        end
+    end
+
+end
