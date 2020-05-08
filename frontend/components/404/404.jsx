c66e073dbc1c51f37f2b5227dc1b00f9bf3b40f2
miggs <jmigueldc95@gmail.com>
2020-05-08 11:36:46 -0700

    fixed some styling

--- frontend/components/404/404.jsx
+++ frontend/components/404/404.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const fourOFour = () => (
 
-    <div>
-      brb 1:50pm
+    <div className="four">
+      <div>404 BRO</div>
+      <img src="https://vmsseaglescall.org/wp-content/uploads/2019/10/Screen-Shot-2019-10-25-at-11.23.07-AM-475x260.png"/>
     </div>
 
 
