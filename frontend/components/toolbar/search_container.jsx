021e661bb32975914242ffe1726fede297d47257
miggs <jmigueldc95@gmail.com>
2020-06-20 20:11:57 -0700

    cleanup

--- frontend/components/toolbar/search_container.jsx
+++ frontend/components/toolbar/search_container.jsx
@@ -9,13 +9,10 @@ import { fetchAllStocks } from '../../actions/asset_actions';
 const mapStateToProps = (state) => ({
   currentUser: state.entities.users[state.session.id],
   stocks: state.entities.assets.allStocks
-
-
 });
 
 const mapDispatchToProps = (dispatch) => ({
   fetchAllStocks: () => dispatch(fetchAllStocks()),
-  
 });
 
 
